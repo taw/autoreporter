67b3d7d5d6e02b98ea10a4f5d50c4e8ff194f254
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2014-09-09 21:16:26 +0100

    autoreporter trollop

--- lib/autoreporter.rb
+++ lib/autoreporter.rb
@@ -2,13 +2,24 @@ require "open3"
 require "shellwords"
 
 class Autoreporter
-  def initialize(command)
-    @command = command
+  attr_accessor :delay, :commands, :verbose
+
+  def initialize
     @output = nil
+    @delay = 60
+    @verbose = false
+    @commands = []
   end
 
-  def run_command!
-    @output, status = Open3.capture2e(@command.shelljoin)
+  def run_command(cmd)
+    output, status = Open3.capture2e(cmd)
+    output += "\n" if output != "" and output[-1] != "\n"
+    output = "Running: #{cmd}\n" + output if verbose
+    output
+  end
+
+  def run_commands!
+    @output = @commands.map{|cmd| run_command(cmd)}
   end
 
   def clear_terminal!
@@ -22,12 +33,12 @@ class Autoreporter
   end
 
   def wait_for_condition!
-    sleep 60
+    sleep @delay
   end
 
   def run!
     while true
-      run_command!
+      run_commands!
       display_result!
       wait_for_condition!
     end
